cb856c821815ee744def916dd3eced84c584575e
Sed-Warlock <vlasov.maksim17062@gmail.com>
2022-05-15 16:16:16 +0300

    Mistake

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 #Разбивает числа на десяиые, сотые, тысячные и т.д
 number=int(input('Введите число: '))
 total=''
+num=' '.join(str(number))
+string=num.split()
+index=0
+while index<len(string):
+    if string[index]!='0':
+        total+=string[index]+(len(string)-index-1)*'0'+' + '
+    index+=1
+print(total.rstrip(' + '))
+#Возвращает строкой
