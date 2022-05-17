2ec552e2159716f94720bc7f143b352a878d31ac
Sed-Warlock <vlasov.maksim17062@gmail.com>
2022-05-17 19:46:02 +0300

    Error

--- test.py
+++ test.py
@@ -11,16 +11,5 @@ while index<len(string):
 print(total.rstrip(' + '))
 #Возвращает строкой
 #Разбивает числа на десяиые, сотые, тысячные и т.д
-number=int(input('Введите число: '))
-total=''
-num=' '.join(str(number))
-string=num.split()
-index=0
-while index<len(string):
-    if string[index]!='0':
-        total+=string[index]+(len(string)-index-1)*'0'+' + '
-    index+=1
-print(total.rstrip(' + '))
-#Возвращает строкой
-#Разбивает числа на десяиые, сотые, тысячные и т.д
-print('The end')
+
+print('Good')
